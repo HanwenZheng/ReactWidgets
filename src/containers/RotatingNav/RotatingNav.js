92157c93d6599d5c4395d73c52167bca890fc50c
Alex Zheng <alex.zhenghw@gmail.com>
2021-04-02 13:08:55 +1100

    feature: menu fade, nav bar

--- src/containers/RotatingNav/RotatingNav.js
+++ src/containers/RotatingNav/RotatingNav.js
@@ -1,31 +1,50 @@
 import { React, useState } from "react";
 import { Button } from "react-bootstrap";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { CSSTransition } from "react-transition-group";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faTimes,
+  faDice,
+  faHeadphones,
+  faRecordVinyl,
+} from "@fortawesome/free-solid-svg-icons";
 
 import classes from "./RotatingNav.module.scss";
-import rotate from "../../helper/Effects/rotate.module.scss";
-import fadeIn from "../../helper/Effects/fadeOff.module.scss";
-import fadeOff from "../../helper/Effects/fadeOff.module.scss";
+import { rotate, fadeIn, fadeOff, slide } from "../../helper/Effects/index";
 
 const RotatingNav = (props) => {
   const [menu, setMenu] = useState(false);
   return (
     <div className={classes.RotatingNav}>
       <div className={classes.menu}>
-        <div>
-          <Button
-            variant="primary"
-            onClick={() => {
-              setMenu(!menu);
-            }}
-          >
-            {menu && <FontAwesomeIcon icon={faBars} />}
-            {!menu && <FontAwesomeIcon icon={faTimes} />}
+        <Button
+          variant="primary"
+          onClick={() => {
+            setMenu(!menu);
+          }}
+        >
+          <CSSTransition in={menu} classNames={fadeIn} timeout={1000}>
+            <div>{!menu && <FontAwesomeIcon icon={faBars} />}</div>
+          </CSSTransition>
+          <CSSTransition in={!menu} classNames={fadeOff} timeout={1000}>
+            <div>{menu && <FontAwesomeIcon icon={faTimes} />}</div>
+          </CSSTransition>
+        </Button>
+      </div>
+      <CSSTransition in={menu} classNames={slide} timeout={1000}>
+        <div className={classes.nav}>
+          <Button variant="primary">
+            <FontAwesomeIcon icon={faDice} />
+          </Button>
+          <Button variant="primary">
+            <FontAwesomeIcon icon={faHeadphones} />
+          </Button>
+          <Button variant="primary">
+            <FontAwesomeIcon icon={faRecordVinyl} />
           </Button>
         </div>
-      </div>
+      </CSSTransition>
       <CSSTransition in={menu} timeout={1000} classNames={rotate}>
         <div className={classes.content}>{props.children}</div>
       </CSSTransition>
---
+++ src/helper/Effects/index.js
@@ -0,0 +1,5 @@
+import fadeIn from "./fadeIn.module.scss";
+import fadeOff from "./fadeOff.module.scss";
+import rotate from "./rotate.module.scss";
+import slide from "./slide.module.scss";
+export { fadeIn, fadeOff, rotate, slide };
